0cab381b52ce20e25b6256ee8842e810fc509868
34735 <zhanggong@rainbowcn.com>
2019-01-28 17:28:59 +0800

    echoServer增加注释

--- example/src/main/java/io/netty/example/echo/EchoServer.java
+++ example/src/main/java/io/netty/example/echo/EchoServer.java
@@ -49,33 +49,48 @@ public final class EchoServer {
         }
 
         // Configure the server.
+        // 创建boss线程组 用于服务端接受客户端的连接。
         EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        // 创建worker线程组，用于进行socketChannel的数据读写。
         EventLoopGroup workerGroup = new NioEventLoopGroup();
-        final EchoServerHandler serverHandler = new EchoServerHandler();
+
+        // final EchoServerHandler serverHandler = new EchoServerHandler();
         try {
-            ServerBootstrap b = new ServerBootstrap();
-            b.group(bossGroup, workerGroup)
-             .channel(NioServerSocketChannel.class)
-             .option(ChannelOption.SO_BACKLOG, 100)
-             .handler(new LoggingHandler(LogLevel.INFO))
-             .childHandler(new ChannelInitializer<SocketChannel>() {
-                 @Override
-                 public void initChannel(SocketChannel ch) throws Exception {
-                     ChannelPipeline p = ch.pipeline();
-                     if (sslCtx != null) {
-                         p.addLast(sslCtx.newHandler(ch.alloc()));
-                     }
-                     //p.addLast(new LoggingHandler(LogLevel.INFO));
-                     p.addLast(serverHandler);
-                 }
-             });
+            // ServerBootstrap netty服务端启动类 引导程序。原来的是链式设置，不容易看明白，我给拆开了。
+            ServerBootstrap bootstrap = new ServerBootstrap();
+            // 设置连接线程组，和数据处理线程组
+            bootstrap.group(bossGroup, workerGroup);
+            // 设置要被实例化的为 NioServerSocketChannel 类
+            bootstrap.channel(NioServerSocketChannel.class);
+            // 设置NioServerSocketChannel 的可选项
+            bootstrap.option(ChannelOption.SO_BACKLOG, 100);
+            // 设置处理器， LoggingHandler，netty提供的handler。它继承ChannelDuplexHandler，是一个双向处理器，用来打印日志。
+            bootstrap.handler(new LoggingHandler(LogLevel.INFO));
+            // ChannelInitializer用来初始化连入服务端的channel
+            bootstrap.childHandler(new ChannelInitializer<SocketChannel>() {
+                        // 设置连入服务端的client的socketChannel 的处理器。
+                        @Override
+                        public void initChannel(SocketChannel ch) throws Exception {
+
+                            ChannelPipeline p = ch.pipeline();
+                            if (sslCtx != null) {
+                                p.addLast(sslCtx.newHandler(ch.alloc()));
+                            }
+                            //p.addLast(new LoggingHandler(LogLevel.INFO));
+                            p.addLast(new EchoServerHandler());
+                        }
+                    });
 
             // Start the server.
-            ChannelFuture f = b.bind(PORT).sync();
+            // 启动服务器，并阻塞等待连接成功。
+            ChannelFuture f = bootstrap.bind(PORT).sync();
 
             // Wait until the server socket is closed.
+            // 监听客户端关闭，并阻塞等待。
             f.channel().closeFuture().sync();
         } finally {
+
+            // 两个线程组的优雅关闭
             // Shut down all event loops to terminate all threads.
             bossGroup.shutdownGracefully();
             workerGroup.shutdownGracefully();
